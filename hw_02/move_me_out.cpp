3be6e8becb708ebe4365d5b2a30d99268ec6b129
mk4cv <lich.mk@gmail.com>
2026-01-31 15:34:58 +0000

    add homework №2

---
+++ hw_02/move_me_out.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void todo_move_me_out() {
+	std::cout << "todo_move_me_out function test" << std::endl;
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
